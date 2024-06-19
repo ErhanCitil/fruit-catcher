cf22999b65422a80f457742c907cff81077d59a7
ErhanCitil <erhancitil94@gmail.com>
2024-06-19 09:06:30 +0200

    🎨 [#1] Added the logic of fruits falling from the top of the screen & added comments

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialiseer Pygame
 pygame.init()
@@ -22,10 +23,14 @@ apple_img = pygame.image.load("Bijlagen voor de kandidaat/Fruits/apple.png")
 cherry_img = pygame.image.load("Bijlagen voor de kandidaat/Fruits/cherry.png")
 pear_img = pygame.image.load("Bijlagen voor de kandidaat/Fruits/pear.png")
 
+fruit_speed = 5
+
+# Hier zorgen we er voor dat de logo 100 bij 100 pixels is
 logo_width = 100 
 logo_height = 100 
 logo_img = pygame.transform.scale(logo_img, (logo_width, logo_height))
 
+# Hier zetten we de instellingen voor de fruitmand
 basket_width = fruit_basket.get_width()
 basket_height = fruit_basket.get_height()
 basket_x = screen_width // 2 - basket_width // 2
@@ -69,8 +74,8 @@ def main_menu():
         button_y = screen_height // 2 - start_button_img.get_height() // 2
         screen.blit(start_button_img, (button_x, button_y))
 
-        logo_x = screen_width - logo_img.get_width() - 10  # 10 pixels van de rand
-        logo_y = 10  # 10 pixels van de bovenkant
+        logo_x = screen_width - logo_img.get_width() - 10
+        logo_y = 10
         screen.blit(logo_img, (logo_x, logo_y))
 
         pygame.display.update()
@@ -80,6 +85,14 @@ def main_menu():
 def game_loop():
     running = True
     global basket_x
+
+    # Python list om de fruit afbeeldingen in op te slaan
+    fruits = []
+    fruit_images = [apple_img, cherry_img, pear_img]
+
+    # Bepaalt na hoeveel frames er een nieuw fruit wordt neergevallen
+    next_fruit_time = random.randint(20, 50)
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -91,8 +104,28 @@ def game_loop():
         if keys[pygame.K_RIGHT] and basket_x < screen_width - basket_width:
             basket_x += basket_speed
 
+        # Nieuwe fruitstukken toevoegen
+        next_fruit_time -= 1
+        if next_fruit_time <= 0:
+            fruit_x = random.randint(0, screen_width - 50)  # willekeurige x-positie
+            fruit_img = random.choice(fruit_images)  # willekeurige fruitafbeelding
+            fruits.append({"img": fruit_img, "x": fruit_x, "y": -50})  # startpositie boven het scherm
+            next_fruit_time = random.randint(20, 50)  # reset de timer
+
+        # Beweeg fruitstukken naar beneden
+        for fruit in fruits:
+            fruit["y"] += fruit_speed
+
+        # Verwijder fruitstukken die onderaan het scherm zijn
+        fruits = [fruit for fruit in fruits if fruit["y"] < screen_height]
+
+        # Tekent de achtergrond nadat je op play button hebt geklikt en de fruitmand op het scherm
         screen.blit(background, (0, 0))
         screen.blit(fruit_basket, (basket_x, basket_y))
+
+        # Teken fruitstukken
+        for fruit in fruits:
+            screen.blit(fruit["img"], (fruit["x"], fruit["y"]))
 
         pygame.display.update()
 
